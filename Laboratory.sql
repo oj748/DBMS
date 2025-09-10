47836f0edf9c1ccefda6263513f2861aade849c4
oj748 <oakojaswini@gmail.com>
2025-09-10 18:43:43 +0530

    Update Laboratory.sql

--- Laboratory.sql
+++ Laboratory.sql
@@ -9,4 +9,8 @@ INSERT INTO Laboratory (Lab_No, Lab_Floor) VALUES
 (3, 2),
 (4, 2);
 
+
+-- Joins
+SELECT B.*, L.Lab_FLoor FROM BloodBank B JOIN Laboratory L ON L.Lab_No = B.Lab_No WHERE L.Lab_No = 2;
+
 SELECT * FROM Laboratory;
