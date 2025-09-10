737c6b4e86cc02a93fec880fe1350b7651b4a6c5
oj748 <oakojaswini@gmail.com>
2025-09-10 18:43:21 +0530

    Update Hospital.sql

--- Hospital.sql
+++ Hospital.sql
@@ -7,9 +7,23 @@ CREATE TABLE IF NOT EXISTS Hospital (
     BB_MGR_ID INT,
     foreign key (BB_MGR_ID) references BloodBankManager(M_ID)
 );
--- DROP TABLE Hospital;
+
 INSERT INTO Hospital (H_ID, H_Name, H_Email, H_Contact, H_Address,BB_MGR_ID) VALUES
 (1, 'City Care Hospital', 'citycare@mail.com, info@citycare.com', '9876543210, 9123456780', '123 Main Street, Mumbai',1),
 (2, 'Green Valley Clinic', 'greenvalley@mail.com', '9988776655', '45 Park Avenue, Delhi',2),
 (3, 'Sunrise Health Center', 'sunrise@mail.com, contact@sunrise.com', '8899776655, 7766554433', '78 Elm Road, Pune',1);
 SELECT * FROM Hospital ;
+
+-- DDL Commands
+ALTER TABLE Hospital MODIFY H_Address varchar(100); 
+-- DML Commands
+
+UPDATE Hospital SET H_Address = '101 Park Square, Mumbai' WHERE H_ID = 1;
+SELECT * FROM Hospital ;
+
+-- DCL Commands
+
+GRANT UPDATE,DELETE ON Hospital TO 'LauraMartinez'@'localhost','RobertTaylor'@'localhost';
+
+-- TCL Commands
+COMMIT;
