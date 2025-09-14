f93b9c58f6b631b5c44b713ce4a9cefd48f168c2
palak16-code <palak.kapoor017@gmail.com>
2025-09-14 20:41:12 +0530

    Updated BloodBankManager.sql

--- BloodBankManager.sql
+++ BloodBankManager.sql
@@ -1,45 +1,38 @@
+-- DDL command
 CREATE TABLE BloodBankManager (
-    M_ID INT PRIMARY KEY,
+    M_ID INT,
     M_Name VARCHAR(100),
     M_Email VARCHAR(100),
-    M_Phone VARCHAR(50)
+    M_Contact VARCHAR(50),
+    PRIMARY KEY (M_ID, M_Email, M_Contact)
 );
 
-INSERT INTO BloodBankManager (M_ID, M_Name, M_Email, M_Contact)
-VALUES
-(1, 'Rajesh Kumar', 'rajesh.kumar@mail.com, rkumar@work.com', '9876543210, 9123456780'),
-(2, 'Anita Singh', 'singh.ani@mail.com', '9988776655');
-
-
--- DDL command 
-ALTER TABLE BloodBankManager
-RENAME COLUMN M_Phone TO M_Contact; -- Renaming Column of table
-
 -- DML commands
-SELECT * FROM BloodBankManager ; -- displaying the entire table data
+INSERT INTO BloodBankManager (M_ID, M_Name, M_Email, M_Contact)VALUES
+(2, 'Anita Singh', 'anita.singh@mail.com', '9988776655');
+SELECT * FROM BloodBankManager;
 
-UPDATE BloodBankManager				-- Modifying data in the table
-SET M_Email = 'anita.singh@mail.com' 
-WHERE M_ID = 2;
+-- DDL command and TCL command
+SAVEPOINT before_truncate;
+TRUNCATE TABLE BloodBankManager;
+SELECT * from BloodBankManager; 		
+ALTER TABLE BloodBankManager MODIFY M_Contact BIGINT;
 
-SELECT * FROM BloodBankManager ;
-
--- DCL Command
-
-REVOKE INSERT ON BloodBankManager 
-FROM 'RobertTaylor'@'localhost','LauraMartinez'@'localhost';
-
-SAVEPOINT before_update; -- Creating a savepoint 
-
-UPDATE BloodBankManager 
-SET M_Name = 'Dr. Sharma' 
-WHERE M_ID = 1;
-
-SELECT * FROM BloodBankManager ;
-
-ROLLBACK TO before_update;  -- Undoing the Update Operation on M_ID = 1
-
-SELECT * FROM BloodBankManager ;
+-- Inserting correct values
+-- DML commands
+INSERT INTO BloodBankManager (M_ID, M_Name, M_Email, M_Contact)VALUES
+(1, 'Rajesh Kumar', 'rajesh.kumar@mail.com', '9876543210'),
+(1, 'Rajesh Kumar', 'rajesh.kumar@mail.com', '9123456780'),
+(1, 'Rajesh Kumar', 'rkumar@work.com', '9876543210'),
+(1, 'Rajesh Kumar', 'rkumar@work.com', '9123456780'),
+(2, 'Anita Singh', 'anita.singh@mail.com', '9988776655');
+SELECT * FROM BloodBankManager;
 
 -- TCL command
-COMMIT; -- Saving all the transactions 
+ROLLBACK TO before_truncate;
+SELECT * from BloodBankManager; 		
+COMMIT;
+
+-- DCL 
+GRANT SELECT, UPDATE, DELETE ON BloodBankManager TO 'RajeshKumar'@'localhost','AnitaSingh'@'localhost';
+REVOKE DELETE ON BloodBankManager FROM 'AnitaSingh'@'localhost';
