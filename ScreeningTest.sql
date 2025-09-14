a4ba4ad4ff3c47689659513c386881e29e8e4a28
palak16-code <palak.kapoor017@gmail.com>
2025-09-14 20:45:19 +0530

    Updated ScreeningTest.sql

--- ScreeningTest.sql
+++ ScreeningTest.sql
@@ -1,5 +1,6 @@
+-- DDL commands
 CREATE TABLE ScreeningTest (
-    Test_ID INT PRIMARY KEY,
+    Test_ID INT,
     D_ID INT,
     BP VARCHAR(20),
     Weight DECIMAL(5,2),
@@ -10,44 +11,37 @@ CREATE TABLE ScreeningTest (
     Eligible BOOLEAN,
     FOREIGN KEY (D_ID) REFERENCES Donor(D_ID)
 );
+
+-- DML commands
+INSERT INTO ScreeningTest 
+(Test_ID, D_ID, BP, Weight, Platelet, Hemoglobin, Hematocrit, Blood_Grp, Eligible) VALUES
+(1, 1, '120/80', 70.50, '250000', 14.5, 42.0, 'A+', TRUE),
+(2, 2, '118/79', 65.20, '240000', 13.8, 40.5, 'B+', TRUE),
+(3, 3, '122/81', 80.00, '270000', 15.2, 43.0, 'O+', TRUE);
+SELECT * FROM ScreeningTest LIMIT 99;
+
+-- DML command (inserting correct values)
 INSERT INTO ScreeningTest (Test_ID, D_ID, BP, Weight, Platelet, Hemoglobin, Hematocrit, Blood_Grp, Eligible) VALUES
 (1, 1, '120/80', 70.50, '250000', 14.5, 42.0, 'A+', TRUE),
 (2, 2,'118/79', 65.20, '240000', 13.8, 40.5, 'B+', TRUE),
 (3, 3, '122/81', 80.00, '270000', 15.2, 43.0, 'O+', TRUE),
 (4, 4, '115/75', 55.50, '230000', 12.9, 38.0, 'AB+', TRUE),
 (5, 5, '125/85', 72.30, '260000', 14.8, 41.5, 'A-', TRUE),
-(6, 6, '119/78', 68.00, '245000', 13.7, 40.0, 'B-', TRUE),
-(7, 7, '121/80', 75.20, '280000', 15.5, 44.0, 'O-', TRUE),
-(8, 8, '117/76', 62.50, '235000', 13.2, 39.0, 'AB-', TRUE),
-(9, 9, '123/82', 78.00, '270000', 14.9, 42.5, 'A+', TRUE),
-(10, 10, '120/79', 69.50, '250000', 14.1, 41.0, 'B+', TRUE),
-(11, 9, '123/82', 78.00, '200000', 14.9, 42.5, 'A+', FALSE);
-SELECT * FROM ScreeningTest;
+(6, 6, '119/78', 68.00, '245000', 13.7, 40.0, 'B-', TRUE);
+SELECT * FROM ScreeningTest LIMIT 99;
 
--- DDL Commands 
-ALTER TABLE ScreeningTest MODIFY Platelet BIGINT;
+-- DML command
+UPDATE ScreeningTest  SET Hemoglobin = 15.0, Eligible = FALSE WHERE Test_ID = 3;
+DELETE FROM ScreeningTest  WHERE Test_ID = 2;
+SELECT * FROM ScreeningTest LIMIT 99;
 
--- DML Commands
-SELECT * FROM ScreeningTest;
-DELETE FROM ScreeningTest WHERE Test_ID = 11;
-SELECT * FROM ScreeningTest;
+-- TCL commands
+SAVEPOINT sp1;
+UPDATE ScreeningTest SET Weight = 72.0 WHERE Test_ID = 1;
+SELECT * FROM ScreeningTest LIMIT 99;
+ROLLBACK TO sp1; 
+SELECT * FROM ScreeningTest LIMIT 99;
+COMMIT;            
 
-
--- DCL Commands
-
-GRANT UPDATE,DELETE ON ScreeningTest TO 'LauraMartinez'@'localhost','RobertTaylor'@'localhost';
-
--- TCL Commands
-
-SAVEPOINT before_delete; 			-- Creating savepoint 
-DELETE FROM ScreeningTest WHERE D_ID BETWEEN 2 AND 5; -- Deleting records
-SELECT * from ScreeningTest;
-
-ROLLBACK TO before_delete;			-- Rollback to previously created savepoint
-SELECT * from ScreeningTest; 		
-
-COMMIT;
-
-
-
-
+-- TCL
+GRANT SELECT, UPDATE, DELETE ON ScreeningTest TO 'RajeshKumar'@'localhost','AnitaSingh'@'localhost';
