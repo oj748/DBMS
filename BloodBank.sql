a4df80d8564aad2e21be137acb5350b77eacdfb6
palak16-code <palak.kapoor017@gmail.com>
2025-09-14 20:40:52 +0530

    Updated BloodBank.sql

--- BloodBank.sql
+++ BloodBank.sql
@@ -1,42 +1,37 @@
+-- DDL commands
 CREATE TABLE IF NOT EXISTS BloodBank (
     Test_ID INT, 
     Lab_No INT,
     B_Spec_No INT PRIMARY KEY,
-    Blood_Grp VARCHAR(5),
+    Blood_Grp VARCHAR(5), 
     B_Quantity INT,
-    B_Date DATE,
-    FOREIGN KEY (Test_ID) REFERENCES ScreeningTest(Test_ID),
-    FOREIGN KEY (Lab_No) REFERENCES Laboratory(Lab_No)
+    B_Date DATE
 );
--- DROP TABLE BloodBank;
 
-INSERT INTO BloodBank
-(Test_ID, Lab_No, B_Spec_No, Blood_Grp, B_Quantity, B_Date)
-VALUES
+-- DML commands (inserting values)
+INSERT INTO BloodBank (Test_ID, Lab_No, B_Spec_No, Blood_Grp, B_Quantity, B_Date) VALUES
 (1, 1, 101, 'A+', 15, '2025-08-01'),
 (2, 2, 102, 'A+', 8, '2025-08-02'),
 (3, 3, 103, 'AB+', 27, '2025-08-03'),
 (4, 1, 104, 'O+', 7, '2025-08-04'),
 (5, 2, 105, 'B+', 15, '2025-08-05'),
-(6, 3, 106,  'A+', 10, '2025-08-06'),
-(7, 1, 107, 'A+', 8, '2025-08-07'),
-(8, 2, 108, 'B+', 8, '2025-08-08'),
-(9, 3, 109, 'O+', 10, '2025-08-09'),
-(10, 1, 110, 'B+', 7, '2025-08-10');
-
-
+(6, 3, 106, 'A+', 10, '2025-08-06');
 SELECT * FROM BloodBank;
 
--- DDL Command
-ALTER TABLE BloodBank MODIFY B_Quantity DECIMAL(6,2);
-DESCRIBE BloodBank;
+-- UPDATE command
+UPDATE BloodBank  SET B_Quantity = 20, Blood_Grp = 'O+'  WHERE B_Spec_No = 105;
+SELECT * FROM BloodBank;
+-- DELETE command
+DELETE FROM BloodBank  WHERE B_Spec_No = 107;
+SELECT * FROM BloodBank;
 
--- DML Commands
-SELECT * FROM BloodBank WHERE Blood_Grp = 'A+';
-
--- DCL Commands
-GRANT DELETE,UPDATE ON BloodBank TO 'LauraMartinez'@'localhost','RobertTaylor'@'localhost';
-
--- TCL Command
+-- TCL commands
+SAVEPOINT sp1;
+UPDATE BloodBank  SET B_Quantity = 18  WHERE B_Spec_No = 101;
+SELECT * FROM BloodBank;
+ROLLBACK TO sp1; 
+SELECT * FROM BloodBank;
 COMMIT;
 
+-- TCL
+GRANT SELECT, UPDATE, DELETE ON Patient TO 'RajeshKumar'@'localhost','AnitaSingh'@'localhost';
